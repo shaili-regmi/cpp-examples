c01cfbf5c105d63de94bd36f10401a193f3d2ec2
alinen <alinen@savvysine.com>
2021-02-18 11:17:49 -0500

    lecture examples

---
+++ week1/snacks.cpp
@@ -0,0 +1,22 @@
+// Bryn Mawr College, 2021
+// Write a program which reads `snacks.txt` and initializes an array of struct Snack
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+// TODO: Define struct Snack
+
+int main(int argc, char** argv)
+{
+#if WIN32
+   string filename = "../../files/snacks.txt";
+#else
+   string filename = "../files/snacks.txt";
+#endif
+
+   // TODO: Your code here
+}
+
